3c995a7249194bb17e01886ac2a32fa12350e776
Noah Provenzano <noahpro@gmail.com>
2023-10-27 12:17:51 -0400

    feat: :sparkles: board placement and camera detection

---
+++ camera.py
@@ -0,0 +1,179 @@
+from tkinter import CURRENT, W
+import cv2
+import numpy as np
+import pygame
+import sys
+
+
+def is_predominantly_red(pixel):
+    b, g, r = pixel
+    return r > 1.2 * g and r > 1.2 * b
+
+
+def get_and_process_frame(vid):
+    ret, frame = vid.read()
+    # cut out the middle square of the frame
+    height, width, _ = frame.shape
+    board_size = min(height, width)
+    top_left_x = (width - board_size) // 2
+    top_left_y = (height - board_size) // 2
+    board = frame[top_left_y:top_left_y+board_size,
+                  top_left_x:top_left_x+board_size]
+
+    # divide the frame into nxn parts
+    frame = board
+    height, width, _ = frame.shape
+    grid_size = 6
+    frame_area = height * width
+    frame_part_height = height // grid_size
+    frame_part_width = width // grid_size
+    frame_part_area = frame_part_height * frame_part_width
+
+    #  and get average rgb values of that part
+    # get average rgb values of each part
+    avg_values = np.zeros((grid_size, grid_size, 3))
+    num_red_pixels = np.zeros((grid_size, grid_size))
+    red_sections = np.zeros((grid_size, grid_size))
+    for i in range(0, grid_size):
+        for j in range(0, grid_size):
+
+            left_index = j * frame_part_width
+            right_index = (j + 1) * frame_part_width
+            top_index = i * frame_part_height
+            bottom_index = (i + 1) * frame_part_height
+            frame_part = frame[top_index:bottom_index, left_index:right_index]
+
+            frame_part_avg = cv2.mean(frame_part)
+            avg_values[i][j] = frame_part_avg[0:3]
+
+            if is_predominantly_red(frame_part_avg[0:3]):
+                red_sections[i][j] = 1
+
+    display_frame = frame.copy()
+
+    # draw grid lines
+    for i in range(1, grid_size+1):
+        display_frame = cv2.line(display_frame, (i * frame_part_width, 0),
+                                 (i * frame_part_width, height), (0, 0, 0), 2)
+        display_frame = cv2.line(display_frame, (0, i * frame_part_height),
+                                 (width, i * frame_part_height), (0, 0, 0), 2)
+
+    # display a small square of the average color of each part
+    for i in range(0, grid_size):
+        for j in range(0, grid_size):
+            # draw a small square of the average color of each part
+            display_frame = cv2.rectangle(display_frame, (j * frame_part_width, i * frame_part_height), (int((j + 0.1) * frame_part_width), int((i + 0.1) * frame_part_height)), tuple(
+                map(lambda x: int(x), avg_values[i][j])), -1)
+
+            # display if it is red or not
+            display_frame = cv2.putText(display_frame, str(red_sections[i][j]), (
+                j * frame_part_width, (i + 1) * frame_part_height), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+    return red_sections, display_frame
+
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 600
+WIDTH = 600
+HEIGHT = 600
+WHITE = (255, 255, 255)
+# store the starting board as list of cars
+START_CAR_POSITIONS = [
+    [[0, 0], [0, 1]],
+    [[2, 0], [3, 0]],
+    [[5, 0], [5, 1]],
+    [[0, 2], [1, 2]],
+]
+
+WANTED_CAR_POSITIONS = []
+CAR_POSITIONS = []
+
+
+def get_red_sections(car_positions):
+    red_sections = np.zeros((6, 6))
+    for car in car_positions:
+        red_sections[car[0][0]][car[0][1]] = 1
+        red_sections[car[1][0]][car[1][1]] = 1
+    return red_sections
+
+
+def draw_grid(screen, red_sections):
+    grid_size = red_sections.shape[0]
+    for i in range(grid_size):
+        for j in range(grid_size):
+            if red_sections[i][j] == 1:
+                pygame.draw.rect(screen, (255, 0, 0), (j * WIDTH / grid_size,
+                                 i * HEIGHT / grid_size, WIDTH / grid_size, HEIGHT / grid_size))
+            else:
+                pygame.draw.rect(screen, (0, 0, 0), (j * WIDTH / grid_size, i *
+                                 HEIGHT / grid_size, WIDTH / grid_size, HEIGHT / grid_size), 1)
+
+
+def draw_next_button(screen):
+    pygame.draw.rect(screen, (255, 200, 255), (WIDTH + 50, 50, 100, 50), 1, 20)
+    font = pygame.font.SysFont('Arial', 20)
+    text = font.render('Next', True, (0, 0, 0))
+    screen.blit(text, (WIDTH + 50 + 25, 50 + 15))
+
+
+def handle_next_button_click(red_sections):
+    # if board looks like the wanted board, then add one car from the start board to the wanted board
+    message = ""
+    if np.array_equal(red_sections, get_red_sections(WANTED_CAR_POSITIONS)):
+        if len(START_CAR_POSITIONS) > 0:
+            WANTED_CAR_POSITIONS.append(START_CAR_POSITIONS.pop(0))
+            CAR_POSITIONS.append(WANTED_CAR_POSITIONS[-1])
+        else:
+            message = "No more cars left!"
+    else:
+        message = "Board does not look like the wanted board, try again!"
+    return message
+
+
+def draw_message(screen, message):
+    font = pygame.font.SysFont('Arial', 20)
+    text = font.render(message, True, (0, 0, 0))
+    screen.blit(text, (WIDTH + 50, 150))
+
+
+def main():
+    vid = cv2.VideoCapture(0)
+    pygame.init()
+    # allow windowed fullscreen mode
+    screen = pygame.display.set_mode(
+        (SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
+
+    pygame.display.set_caption("Rush Hour Board")
+
+    clock = pygame.time.Clock()
+    message = ""
+    while (True):
+        # openCV stuff
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        red_sections, display_frame = get_and_process_frame(vid)
+        print(get_red_sections(WANTED_CAR_POSITIONS))
+        cv2.imshow('frame', display_frame)
+
+        # pygame stuff
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONUP:
+                pos = pygame.mouse.get_pos()
+                if pos[0] > WIDTH + 50 and pos[0] < WIDTH + 150 and pos[1] > 50 and pos[1] < 100:
+                    message = handle_next_button_click(red_sections)
+
+        screen.fill(WHITE)
+        draw_next_button(screen)
+        draw_grid(screen, get_red_sections(CAR_POSITIONS))
+        draw_message(screen, message)
+        pygame.display.flip()
+        clock.tick(60)
+
+    vid.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    main()
