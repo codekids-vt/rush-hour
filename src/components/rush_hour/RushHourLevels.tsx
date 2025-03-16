8166acb8f4523a77276b34f38b93f91e8e8c8cec
Peter Murphy <pmurphy26@vt.edu>
2025-03-16 14:13:59 -0400

    fixed the cars being able to move through eachother bug and changed some colors that wouldn't render for some reason when the level was loaded

--- src/components/rush_hour/RushHourLevels.tsx
+++ src/components/rush_hour/RushHourLevels.tsx
@@ -14,16 +14,18 @@ export const levels : Car[][] = [[ //intro
   { x: 5, y: 3, vertical: true, length: 3, color: "blue" },
   { x: 4, y: 0, vertical: false, length: 2, color: "green" },
   { x: 2, y: 0, vertical: true, length: 3, color: "yellow" },
-  { x: 0, y: 3, vertical: false, length: 3, color: "orange" }
+  { x: 0, y: 3, vertical: false, length: 3, color: "purple" }
 ],
 [ //intermediate
     { x: 0, y: 2, vertical: false, length: 2, color: "red" },
     { x: 0, y: 4, vertical: false, length: 3, color: "blue" },
     { x: 0, y: 0, vertical: false, length: 3, color: "yellow" },
     { x: 1, y: 5, vertical: false, length: 2, color: "green" },
-    { x: 2, y: 1, vertical: true, length: 3, color: "orange" },
+//    { x: 2, y: 1, vertical: true, length: 3, color: "orange" },
+    { x: 2, y: 1, vertical: true, length: 3, color: "yellow" },
     { x: 3, y: 0, vertical: true, length: 2, color: "green" },
-    { x: 5, y: 0, vertical: true, length: 2, color: "orange" },
+//    { x: 5, y: 0, vertical: true, length: 2, color: "orange" },
+    { x: 5, y: 0, vertical: true, length: 2, color: "purple" },
     { x: 5, y: 2, vertical: true, length: 2, color: "blue" },
     { x: 3, y: 3, vertical: false, length: 2, color: "pink" },
     { x: 4, y: 4, vertical: true, length: 2, color: "purple" }
@@ -35,10 +37,12 @@ export const levels : Car[][] = [[ //intro
     { x: 4, y: 4, vertical: true, length: 2, color: "blue" },
     { x: 5, y: 4, vertical: true, length: 2, color: "pink" },
     { x: 0, y: 0, vertical: false, length: 2, color: "green" },
-    { x: 0, y: 1, vertical: true, length: 3, color: "orange" },
+//    { x: 0, y: 1, vertical: true, length: 3, color: "orange" }, //doesn't render
+    { x: 0, y: 1, vertical: true, length: 3, color: "yellow" },
     { x: 0, y: 4, vertical: false, length: 3, color: "green" },
-    { x: 2, y: 0, vertical: true, length: 2, color: "orange" },
-    { x: 0, y: 5, vertical: false, length: 2, color: "purple" }
+//    { x: 2, y: 0, vertical: true, length: 2, color: "orange" }, //orange doesn't render for some reason
+    { x: 2, y: 0, vertical: true, length: 2, color: "purple" }, 
+    { x: 0, y: 5, vertical: false, length: 2, color: "purple" } 
 ],
 [
     { x: 2, y: 2, vertical: false, length: 2, color: "red" },
@@ -51,7 +55,9 @@ export const levels : Car[][] = [[ //intro
     { x: 1, y: 4, vertical: true, length: 2, color: "green" },
     { x: 2, y: 5, vertical: false, length: 2, color: "blue" }, //300
     { x: 4, y: 5, vertical: false, length: 2, color: "yellow" },
-    { x: 4, y: 0, vertical: true, length: 3, color: "orange" }, //700
-    { x: 0, y: 1, vertical: true, length: 2, color: "orange" }, //700
+    { x: 4, y: 0, vertical: true, length: 3, color: "purple" }, 
+    { x: 0, y: 1, vertical: true, length: 2, color: "yellow" }, 
+//    { x: 4, y: 0, vertical: true, length: 3, color: "orange" }, //700
+//    { x: 0, y: 1, vertical: true, length: 2, color: "orange" }, //700
     { x: 3, y: 4, vertical: false, length: 2, color: "gray" },
   ]]
--- src/isLegalMove.ts
+++ src/isLegalMove.ts
@@ -9,6 +9,57 @@ function carsOverlap(car1: Car, car2: Car): boolean {
     return overlapping;
 }
 
+//change so that it can handle going down to up and right to left
+function carsOnPath(otherCars: Car[], pathTaken: number[]) { //pathTaken = [startX, endX, startY, endY]
+    const movedHorizontal = (pathTaken[2] == pathTaken[3]);
+    
+    for (const car of otherCars) {
+        if (movedHorizontal) {
+            let movedCarY = pathTaken[2];
+            var leftX = pathTaken[0];
+            var rightX = pathTaken[1];
+
+            //swap left and right X if the car was moved from right to left
+            if (rightX < leftX) {
+                var temp = rightX;
+                rightX = leftX;
+                leftX = temp;
+            }
+
+            console.log(leftX);
+            console.log(rightX);
+
+
+            if (car.x < rightX && car.x > leftX) { //other car is located between x coords
+                if (car.y == movedCarY) { //if other car's Y is located along path (covers all horizontal)
+                    return true;
+                }
+
+                //if other car is vertical and located along the path
+                if (car.vertical && car.y < movedCarY && (car.y + car.length) >= movedCarY) {
+                    return true;
+                }
+            }
+        } else { //car moved vertically
+            let movedCarX = pathTaken[0];
+            let bottomY = pathTaken[3];
+            let topY = pathTaken[2];
+
+            if (car.x < bottomY && car.x > topY) { //other car is located between y coords
+                if (car.x == movedCarX) { //if other car's X is located along path (covers all vertical)
+                    return true;
+                }
+
+                //if other car is horizontal and located along the path
+                if (!car.vertical && car.x < movedCarX && (car.x + car.length) >= movedCarX) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+}
 
 export function isLegalMove(cars: Car[], car: Car, newCar: Car): boolean {
     // get all cars but the car being moved
@@ -39,6 +90,12 @@ export function isLegalMove(cars: Car[], car: Car, newCar: Car): boolean {
         }
     }
 
+
+    //check if there were any moved cars along the path that the moved on went through
+    let pathTaken = [car.x, newCar.x, car.y, newCar.y];
+    if (carsOnPath(otherCars, pathTaken)) {
+        return false;
+    }
 
 
     return true;
